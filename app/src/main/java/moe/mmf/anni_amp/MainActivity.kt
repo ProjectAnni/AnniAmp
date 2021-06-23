27bf1c3adfe0b4684680f144b010b883a418d876
Yesterday17 <mmf@mmf.moe>
2021-06-23 11:40:48 +0800

    Project Structure.

---
+++ app/src/main/java/moe/mmf/anni_amp/MainActivity.kt
@@ -0,0 +1,11 @@
+package moe.mmf.anni_amp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
